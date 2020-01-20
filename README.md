32e117d79464ca973abf7b149ca58f188e585c07
a6hinavram <a6hinavram@gmail.com>
2020-01-20 13:12:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zremww
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zremww)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  Hello {{title}}
+`
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
